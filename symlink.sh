da33da4dac7bbe37e25b4cc7d0f9af729b0738bb
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-08-13 17:14:31 +0300

    [symlink.sh] fix: if $HOME does not contain /home/ (as root user)

--- symlink.sh
+++ symlink.sh
@@ -2,8 +2,11 @@
 # create relative symlink to dir [$XDG_CONFIG_HOME]
 ABSP="$(realpath "$0")" # get absolute full path of current script
 BD="$(dirname "$ABSP")" # base directory path of current script
-home_path_hack="$(echo "$BD" | grep -o "/home/\w*")" # we assume that $BD is inside users home directory
-HOME="${HOME-"$home_path_hack"}" # if $HOME is empty use $home_path_hack
-[ "$HOME" = "/root" ] && HOME="$home_path_hack" # fix: if $HOME is "/root" (root user)
+case "$HOME" in
+    /home/*) ;; # if $HOME contains "/home/" -> regular user
+    # fix for: root user $HOME=/root etc.
+    # HACK: we assume that $BD is inside users home directory
+    *) HOME="$(echo "$BD" | grep -o "/home/\w*")" ;;
+esac
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 ln -rs "$BD" "$XDG_CONFIG_HOME" # create relative symlink to dir
