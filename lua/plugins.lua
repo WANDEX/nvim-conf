b2461c1a8692d33ee5839ef6c2d8084bd09e86f2
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-08-29 23:59:53 +0300

    added: lua/plugins.lua - packer.nvim plugins.lua file

---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+local use = require('packer').use
+require('packer').startup(function()
+    -- Packer can manage itself
+    use {'wbthomason/packer.nvim', opt = true}
+end)
+
