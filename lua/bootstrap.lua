c00ccd244a2f63d87de6d8c29587d747f8ba516a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-08-29 23:58:52 +0300

    added: lua/bootstrap.lua - auto bootstrap packer.nvim package

---
+++ lua/bootstrap.lua
@@ -0,0 +1,8 @@
+-- automatically install/bootstrap packer.nvim package (for lua plugins management)
+-- should be sourced via: `lua require 'bootstrap'` before our first usage of packer in configs!
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
