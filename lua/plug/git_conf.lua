55258efbbe39d72e0fbdb6ea28005df548da1a76
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-08 14:07:51 +0300

    [git_conf.lua] added gitsigns configuration

--- lua/plug/git_conf.lua
+++ lua/plug/git_conf.lua
@@ -2,15 +2,30 @@
 
 local use = require('packer').use
 use {
-  'jreybert/vimagit',
   'tpope/vim-fugitive',
+  'jreybert/vimagit', -- till neogit is not fixed
 }
 
 use {
+  {'TimUntersberger/neogit', disable=true}, -- FIXME throws error if enabled
   'lewis6991/gitsigns.nvim',
   requires = {
     'nvim-lua/plenary.nvim',
-  }
+  },
 }
 
-require('gitsigns').setup()
+require('gitsigns').setup{
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '^', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+  numhl = false,
+  current_line_blame_formatter_opts = {
+    relative_time = true,
+  },
+}
+
+-- require('neogit').setup{}
