86d27df4d060a510daaa644bff0cbbd0ab8c114e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-16 15:35:50 +0300

    [auto.lua] +augroup packer_user_config

--- lua/auto.lua
+++ lua/auto.lua
@@ -2,5 +2,10 @@
 -- autogroups and autocommands
 
 -- run PackerCompile command every time when any of the nvim/lua/* config files are written
-vim.cmd([[au BufWritePost */nvim/lua/* so <afile> | PackerCompile]])
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost */nvim/lua/* source <afile> | PackerCompile
+  augroup end
+]])
 
