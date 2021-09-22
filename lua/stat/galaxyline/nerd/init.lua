703c2ddbb068f6a8c1a8322e800531e295701a0a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-22 14:46:53 +0300

    [nerd] removed all COC related code

--- lua/stat/galaxyline/nerd/init.lua
+++ lua/stat/galaxyline/nerd/init.lua
@@ -37,11 +37,6 @@ local colors = {
   red      = '#D54E53',
 }
 
-local use_coc = false
-if vim.g.nerd_galaxyline_lsp == 'coc' then
-  use_coc = true
-end
-
 local function lsp_status(status)
   local shorter_stat = ''
   for match in string.gmatch(status, "[^%s]+")  do
@@ -53,34 +48,6 @@ local function lsp_status(status)
   return shorter_stat
 end
 
-local function get_coc_lsp()
-  local status = vim.fn['coc#status']()
-  if not status or status == '' then
-    return ''
-  end
-  return lsp_status(status)
-end
-
-local function get_diagnostic_info()
-  if vim.fn.exists('*coc#rpc#start_server') == 1 then
-    return get_coc_lsp()
-  end
-  return ''
-end
-
-local function get_current_func()
-  local has_func, func_name = pcall(vim.fn.nvim_buf_get_var,0,'coc_current_function')
-  if not has_func then return end
-  return func_name
-end
-
-local function get_function_info()
-  if vim.fn.exists('*coc#rpc#start_server') == 1 then
-    return get_current_func()
-  end
-  return ''
-end
-
 local function trailing_whitespace()
   local trail = vim.fn.search("\\s$", "nw")
   if trail ~= 0 then
@@ -90,8 +57,6 @@ local function trailing_whitespace()
   end
 end
 
-CocStatus = get_diagnostic_info
-CocFunc = get_function_info
 TrailingWhiteSpace = trailing_whitespace
 
 local function has_file_type()
@@ -327,36 +292,18 @@ insert_left {
 }
 
 insert_left {
-  CocStatus = {
-    provider = CocStatus,
-    highlight = {colors.green,colors.line_bg},
-    icon = '  ',
-    condition = use_coc,
-  }
-}
-
-insert_left {
-  CocStatus = {
+  DiagnosticInfo = {
     provider = 'DiagnosticInfo',
     highlight = {colors.green,colors.line_bg},
     icon = '  ',
-    condition = function() return checkwidth() and not use_coc end,
-  }
-}
-
-insert_left {
-  CocFunc = {
-    provider = CocFunc,
-    icon = ' λ ',
-    highlight = {colors.yellow,colors.line_bg},
-    condition = use_coc,
+    condition = function() return checkwidth() end,
   }
 }
 
 insert_left {
   DiagnosticHint = {
     provider = 'DiagnosticHint',
-    condition = function() return checkwidth() and not use_coc end,
+    condition = function() return checkwidth() end,
     highlight = {colors.white,colors.line_bg},
     icon = '  ',
   }
