670b84e309ed8cd759385871d99201f078282d03
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-29 14:48:41 +0300

    [plugins.lua] sort text object plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,11 +25,11 @@ require('packer').startup({function()
 
   use { -- new text objects | more info: 'https://github.com/kana/vim-textobj-user/wiki'
     'kana/vim-textobj-user',    -- CORE (wont be installed with 'requires = ...')
-    'kana/vim-textobj-indent',  -- ai/ii aI/iI
-    'kana/vim-textobj-line',    -- al/il
-    'kana/vim-textobj-entire',  -- ae/ie
     'glts/vim-textobj-comment', -- ac/ic aC
     'kana/vim-textobj-diff',    -- adh/idh
+    'kana/vim-textobj-entire',  -- ae/ie
+    'kana/vim-textobj-indent',  -- ai/ii aI/iI
+    'kana/vim-textobj-line',    -- al/il
     {'bps/vim-textobj-python', ft='python'}, -- af/if ac/ic [pf/]pf [pc/]pc
   }
 
