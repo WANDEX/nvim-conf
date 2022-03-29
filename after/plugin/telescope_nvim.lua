c750f883626f88cf78024b6b222ba2b24f7b8492
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-03-29 16:23:13 +0300

    [telescope_nvim.lua] defaults, layout dimensions, toggle_preview

--- after/plugin/telescope_nvim.lua
+++ after/plugin/telescope_nvim.lua
@@ -5,12 +5,59 @@ if not ok then
   return
 end
 
-local actions = require('telescope.actions')
+local actions = require 'telescope.actions'
+local action_layout = require 'telescope.actions.layout'
+
+local flip_cols = 110
 
 telescope.setup {
   defaults = {
+    prompt_prefix = "> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    multi_icon = "<>",
+
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    scroll_strategy = "cycle",
+    color_devicons = true,
+
+    winblend = 0,
+
+    layout_strategy = "flex",
+    layout_config = {
+      width = 0.9,
+      height = 0.95,
+      prompt_position = "bottom",
+
+      horizontal = {
+        preview_cutoff = 1, -- always show Preview (unless previewer = false)
+        preview_width = 0.5,
+--[[ -- or More complex:
+        preview_width = function(_, cols, _)
+          if cols < flip_cols then
+            return math.floor(cols * 0.4)
+          else
+            return math.floor(cols * 0.5)
+          end
+        end,
+ ]]
+      },
+
+      vertical = {
+        preview_cutoff = 1, -- always show Preview (unless previewer = false)
+        preview_height = 0.45,
+      },
+
+      flex = {
+        flip_columns = flip_cols,
+      },
+    },
+
     mappings = { -- Global remapping
       i = {
+        ["<M-p>"] = action_layout.toggle_preview,
+
         ["<C-n>"] = actions.move_selection_next,
         ["<C-e>"] = actions.move_selection_previous,
         ["<C-p>"] = false,
@@ -28,11 +75,10 @@ telescope.setup {
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
+      fuzzy = true,                     -- false will only do exact matching
+      override_generic_sorter = false,  -- override the generic sorter
+      override_file_sorter = true,      -- override the file sorter
+      case_mode = "smart_case",         -- "ignore_case" or "respect_case" or "smart_case"
     },
     tele_tabby = {
       use_highlighter = true,
@@ -40,5 +86,5 @@ telescope.setup {
   },
 }
 
-telescope.load_extension 'fzf' -- fzf-native
+telescope.load_extension 'fzf'          -- fzf-native
 telescope.load_extension 'file_browser'
