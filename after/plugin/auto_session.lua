3756a53e1edc83af72551cccaf7a326099eb6bc3
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-03-15 13:21:32 +0300

    [auto_session.lua] upd

--- after/plugin/auto_session.lua
+++ after/plugin/auto_session.lua
@@ -11,13 +11,14 @@ if not aul_ok then
 end
 
 -- Recommended sessionoptions config
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 local opts = {
   log_level = 'info',
   auto_session_enable_last_session = false,
   auto_session_root_dir = vim.fn.stdpath('data').."/aus/",
   auto_session_enabled = true,
+  auto_session_create_enabled = false,
   auto_save_enabled = false,
   auto_restore_enabled = true,
   -- auto_session_allowed_dirs = ["list", "of paths"],
@@ -28,6 +29,7 @@ aus.setup(opts)
 
 aul.setup {
   path_display = {'shorten'},
-  theme_conf = { winblend = 0, border = true },
-  previewer = false
+  theme = 'dropdown',
+  theme_conf = { border = false },
+  previewer = false,
 }
