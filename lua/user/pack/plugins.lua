129a844abcf2d07adae6f7ee8e86b2cf3e548603
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-12-28 15:45:08 +0300

    [plugins.lua] added: 'glacambre/firenvim'

--- lua/user/pack/plugins.lua
+++ lua/user/pack/plugins.lua
@@ -221,6 +221,11 @@ packer.startup(function()
     },
   }
 
+  use {
+    'glacambre/firenvim',
+    run = function() vim.fn['firenvim#install'](0) end,
+  }
+
   -- at the end
   use 'neomake/neomake'
   use 'folke/which-key.nvim'
