23a4f313e161aac55e16b513b1f081ce1430779b
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-05-18 21:16:23 +0300

    [which_key.lua] upd which-key config for the up-to-date spec

--- after/plugin/which_key.lua
+++ after/plugin/which_key.lua
@@ -6,6 +6,7 @@ if not ok then
 end
 
 wk.setup {
+  notify = false, -- TODO: toggle after updates | show a warning when issues were detected with your mappings
   plugins = {
     spelling = {
       enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
@@ -16,167 +17,164 @@ wk.setup {
       windows = false, -- disable default bindings help on <c-w> -> because default keys are remapped
     },
   },
-  -- ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 }
 
--- window mappings (because default keys are remapped)
-wk.register({ ["<C-W>"] = {
-  name = "window",
-  ["+"] = "Increase height",
-  ["-"] = "Decrease height",
-  ["="] = "Equally high and wide",
-  [">"] = "Increase width",
-  ["<"] = "Decrease width", -- bug? - it's not showing.
-  ["|"] = "Max out the width",
-  ["<C-N>"] = "New empty buffer window",
+wk.add({ -- window mappings (because default keys are remapped for the colemak layout)
+  { "<C-W>", group = "window" },
+  { "<C-W>+", desc = "Increase height" },
+  { "<C-W>-", desc = "Decrease height" },
+  { "<C-W>>", desc = "Increase width" },
+  { "<C-W><", desc = "Decrease width" }, -- XXX: bug? - it's not showing.
+  { "<C-W>|", desc = "Max out the width" },
+  { "<C-W>=", desc = "Equally high and wide" },
+  { "<C-W><C-N>", desc = "New empty buffer window" },
 
-  ["h"] = "Go to the left window",
-  ["n"] = "Go to the down window",
-  ["e"] = "Go to the up window",
-  ["i"] = "Go to the right window",
+  { "<C-W>h", desc = "Go to the left window" },
+  { "<C-W>n", desc = "Go to the down window" },
+  { "<C-W>e", desc = "Go to the up window" },
+  { "<C-W>i", desc = "Go to the right window" },
 
-  ["H"] = "Move window to the left",
-  ["N"] = "Move window to the down",
-  ["E"] = "Move window to the up",
-  ["I"] = "Move window to the right",
+  { "<C-W>H", desc = "Move window to the left" },
+  { "<C-W>N", desc = "Move window to the down" },
+  { "<C-W>E", desc = "Move window to the up" },
+  { "<C-W>I", desc = "Move window to the right" },
 
-  ["q"] = "Quit a window",
-  ["s"] = "Split window",
-  ["T"] = "Break out into a new tab",
+  { "<C-W>q", desc = "Quit a window" },
+  { "<C-W>s", desc = "Split window" },
+  { "<C-W>T", desc = "Break out into a new tab" },
 
-  ["v"] = "Split window vertically",
-  ["w"] = "Switch windows",
-  ["x"] = "Swap current with next",
-}, })
+  { "<C-W>v", desc = "Split window vertically" },
+  { "<C-W>w", desc = "Switch windows" },
+  { "<C-W>x", desc = "Swap current with next" },
+})
 
--- add
-wk.register({ a = {
-  name = "add/alt",
-  t = { "<cmd>ToggleAlternate<CR>", "toggle alt val" },
-}, }, { prefix = "<leader>" })
+wk.add({ -- add/alt
+  { "<leader>a", group = "add/alt" },
+  { "<leader>at", "<cmd>ToggleAlternate<CR>", desc = "toggle alt val" },
+})
 
-wk.register({ c = {
-  name = "comment",
-}, }, { prefix = "<leader>" })
+wk.add({
+  { "<leader>c", group = "comment" },
+})
 
--- delete
-wk.register({ d = {
-  name = "delete/diff",
-  d = { "<cmd>bd!<CR>", "buffer delete" },
-  W = { "Whitespace strip" },
-}, }, { prefix = "<leader>" })
+wk.add({ -- delete/diff
+  { "<leader>d", group = "delete/diff" },
+  { "<leader>dW", desc = "Whitespace strip" },
+  { "<leader>dd", "<cmd>bd!<CR>", desc = "buffer delete" },
+})
 
-wk.register({ h = {
-  name = "hunk",
-}, }, { prefix = "<leader>" })
+wk.add({
+  { "<leader>h", group = "hunk" },
+})
 
-wk.register({ L = {
-  name = "List",
-}, }, { prefix = "<leader>" })
+wk.add({
+  { "<leader>L", group = "List" },
+})
 
--- LSP - just a label. don't create any mappings
-wk.register({ ["gD"] = "[LSP] declaration" })
-wk.register({ ["gd"] = "[LSP] definition" })
-wk.register({ ["gi"] = "[LSP] implementation" })
-wk.register({ ["gr"] = "[LSP] references" })
-wk.register({ ["<C-k>"] = "[LSP] signature help" })
-wk.register({ ["[d"] = "[LSP] prev diag" })
-wk.register({ ["]d"] = "[LSP] next diag" })
-wk.register({ l = {
-  name = "lsp",
-  a = { "action" },
-  D = { "type definition" },
-  e = { "show/enter line diag" },
-  f = { "formatting" },
-  h = { "hover" },
-  l = { "list diag" },
-  r = { "rename" },
-  w = {
-    name = "workspace",
-    a = { "add workspace folder" },
-    l = { "list workspace folders" },
-    r = { "remove workspace folder" },
+wk.add({ -- LSP - just a label. don't create any mappings
+  { "<C-k>", desc = "[LSP] signature help" },
+  { "gD", desc = "[LSP] declaration" },
+  { "gd", desc = "[LSP] definition" },
+  { "gi", desc = "[LSP] implementation" },
+  { "gr", desc = "[LSP] references" },
+  { "[d", desc = "[LSP] prev diag" },
+  { "]d", desc = "[LSP] next diag" },
+})
+wk.add({ -- lsp
+  { "<leader>l", group = "lsp" },
+  { "<leader>lD", desc = "type definition" },
+  { "<leader>la", desc = "action" },
+  { "<leader>le", desc = "show/enter line diag" },
+  { "<leader>lf", desc = "formatting" },
+  { "<leader>lh", desc = "hover" },
+  { "<leader>ll", desc = "list diag" },
+  { "<leader>lr", desc = "rename" },
+  { "<leader>lw", group = "workspace" },
+  { "<leader>lwa", desc = "add workspace folder" },
+  { "<leader>lwl", desc = "list workspace folders" },
+  { "<leader>lwr", desc = "remove workspace folder" },
+})
+
+wk.add({
+  { "<leader>M", group = "Magit" },
+  { "<leader>Mh", "<cmd>call magit#show_magit('h')<CR>", desc = "hrz" },
+  { "<leader>Mo", "<cmd>call magit#show_magit('c')<CR>", desc = "only" },
+  { "<leader>Mv", desc = "vrt" }, -- magit cannot unbind def mapping
+})
+
+wk.add({
+  { "<leader>N", group = "Neogit" },
+  { "<leader>NS", "<cmd>lua require('neogit').open({ kind = 'split_above' })<CR>", desc = "split above" },
+  { "<leader>Nc", "<cmd>lua require('neogit').open({ 'commit' })<CR>", desc = "commit" },
+  { "<leader>Ns", "<cmd>lua require('neogit').open({ kind = 'split' })<CR>", desc = "split below" },
+  { "<leader>Nt", "<cmd>lua require('neogit').open({ kind = 'tab' })<CR>", desc = "tab" },
+  { "<leader>Nv", "<cmd>lua require('neogit').open({ kind = 'vsplit' })<CR>", desc = "vsplit" },
+})
+
+wk.add({
+  { "<leader>T", group = "Telescope" },
+  { "<leader>TB", "<cmd>lua require'telescope.builtin'.buffers{}<CR>", desc = "buffers w preview" },
+  { "<leader>TD", ":lua require'telescope.builtin'.live_grep({search_dirs={ '', }})<C-Left><C-Left><Right>", desc = "grep in list of dirs" },
+  { "<leader>TF", "<cmd>lua require'telescope'.extensions.file_browser.file_browser{ hidden=false, respect_gitignore=false, }<CR>", desc = "Filesystem" },
+  { "<leader>TS", "<cmd>SearchSession<CR>", desc = "Session" },
+  { "<leader>Tb", "<cmd>Telescope buffers theme=dropdown previewer=false border=false<CR>", desc = "buffers" },
+  { "<leader>Tc", "<cmd>lua require'telescope.builtin'.grep_string{}<CR>", desc = "grep under Cursor at cwd" },
+  { "<leader>Td", "<cmd>Telescope diagnostics<CR>", desc = "diagnostics" },
+  { "<leader>Tf", "<cmd>lua require'telescope.builtin'.find_files{}<CR>", desc = "find file at cwd" },
+  { "<leader>Tg", "<cmd>lua require'telescope.builtin'.live_grep{}<CR>", desc = "grep at cwd" },
+  { "<leader>To", "<cmd>lua require'telescope.builtin'.live_grep{ grep_open_files=true }<CR>", desc = "grep Opened files" },
+  { "<leader>Tt", "<cmd>Telescope tele_tabby list theme=dropdown previewer=false border=false<CR>", desc = "tabs" },
+  { "<leader>Tv", group = "vim" },
+  { "<leader>TvC", "<cmd>lua require'telescope.builtin'.colorscheme{}<CR>", desc = "colorscheme" },
+  { "<leader>TvH", "<cmd>lua require'telescope.builtin'.highlights{}<CR>", desc = "Highlights" },
+  { "<leader>TvT", "<cmd>lua require'telescope.builtin'.current_buffer_tags{}<CR>", desc = "tags buf" },
+  { "<leader>Tvc", "<cmd>lua require'telescope.builtin'.command_history{}<CR>", desc = "command history" },
+  { "<leader>Tvf", "<cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find{}<CR>", desc = "fuzzy find buf" },
+  { "<leader>Tvh", "<cmd>lua require'telescope.builtin'.help_tags{}<CR>", desc = "help tags" },
+  { "<leader>Tvk", "<cmd>Telescope keymaps<CR>", desc = "keymaps" },
+  { "<leader>Tvm", "<cmd>lua require'telescope.builtin'.man_pages{}<CR>", desc = "man pages" },
+  { "<leader>Tvo", "<cmd>lua require'telescope.builtin'.vim_options{}<CR>", desc = "options vim edit" },
+  { "<leader>Tvp", "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>", desc = "prev opened files" },
+  { "<leader>Tvr", "<cmd>lua require'telescope.builtin'.registers{}<CR>", desc = "registers" },
+  { "<leader>Tvs", "<cmd>lua require'telescope.builtin'.search_history{}<CR>", desc = "search history" },
+})
+
+wk.add({ -- translate normal mode
+  {
+    mode = { "n" },
+    { "<leader>t", group = "trans" },
+    { "<leader>t!l", "<cmd>normal V<CR> | :'<,'>TranslateW!<CR>", desc = "line" },
+    { "<leader>t!r", "<cmd>normal V<CR> | :'<,'>TranslateR!<CR>", desc = "replace line" },
+    { "<leader>t!w", "<cmd>TranslateW!<CR>", desc = "word" },
+    { "<leader>tl", "<cmd>normal V<CR> | :'<,'>TranslateW<CR>", desc = "line" },
+    { "<leader>tr", "<cmd>normal V<CR> | :'<,'>TranslateR<CR>", desc = "replace line" },
+    { "<leader>tw", "<cmd>TranslateW<CR>", desc = "word" },
   },
-}, }, { prefix = "<leader>" })
+})
 
-wk.register({ M = {
-  name = "Magit",
-  h = { "<cmd>call magit#show_magit('h')<CR>", "hrz" },
-  o = { "<cmd>call magit#show_magit('c')<CR>", "only" },
-  v =  "vrt", -- magit cannot unbind def mapping
-}, }, { prefix = "<leader>" })
-
-wk.register({ N = {
-  name = "Neogit",
-  c = { "<cmd>lua require('neogit').open({ 'commit' })<CR>", "commit" },
-  s = { "<cmd>lua require('neogit').open({ kind = 'split' })<CR>", "split below" },
-  S = { "<cmd>lua require('neogit').open({ kind = 'split_above' })<CR>", "split above" },
-  t = { "<cmd>lua require('neogit').open({ kind = 'tab' })<CR>", "tab" },
-  v = { "<cmd>lua require('neogit').open({ kind = 'vsplit' })<CR>", "vsplit" },
-}, }, { prefix = "<leader>" })
-
-wk.register({ T = {
-  name = "Telescope",
-  -- command! -nargs=? Tgrep lua require 'telescope.builtin'.grep_string({ search = vim.fn.input("Grep For > ")})
-  b = { "<cmd>Telescope buffers theme=dropdown previewer=false border=false<CR>", "buffers" },
-  B = { "<cmd>lua require'telescope.builtin'.buffers{}<CR>", "buffers w preview" },
-  c = { "<cmd>lua require'telescope.builtin'.grep_string{}<CR>", "grep under Cursor at cwd" },
-  d = { "<cmd>Telescope diagnostics<CR>", "diagnostics" },
-  D = { ":lua require'telescope.builtin'.live_grep({search_dirs={ '', }})<C-Left><C-Left><Right>", "grep in list of dirs" },
-  f = { "<cmd>lua require'telescope.builtin'.find_files{}<CR>", "find file at cwd" },
-  F = { "<cmd>lua require'telescope'.extensions.file_browser.file_browser{ hidden=false, respect_gitignore=false, }<CR>", "Filesystem" },
-  g = { "<cmd>lua require'telescope.builtin'.live_grep{}<CR>", "grep at cwd" },
-  o = { "<cmd>lua require'telescope.builtin'.live_grep{ grep_open_files=true }<CR>", "grep Opened files" },
-  S = { "<cmd>SearchSession<CR>", "Session" },
-  t = { "<cmd>Telescope tele_tabby list theme=dropdown previewer=false border=false<CR>", "tabs" },
-  v = {
-    name = "vim",
-    C = { "<cmd>lua require'telescope.builtin'.colorscheme{}<CR>", "colorscheme" },
-    c = { "<cmd>lua require'telescope.builtin'.command_history{}<CR>", "command history" },
-    f = { "<cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find{}<CR>", "fuzzy find buf" },
-    h = { "<cmd>lua require'telescope.builtin'.help_tags{}<CR>", "help tags" },
-    H = { "<cmd>lua require'telescope.builtin'.highlights{}<CR>", "Highlights" },
-    m = { "<cmd>lua require'telescope.builtin'.man_pages{}<CR>", "man pages" },
-    o = { "<cmd>lua require'telescope.builtin'.vim_options{}<CR>", "options vim edit" },
-    p = { "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>", "prev opened files" },
-    r = { "<cmd>lua require'telescope.builtin'.registers{}<CR>", "registers" },
-    s = { "<cmd>lua require'telescope.builtin'.search_history{}<CR>", "search history" },
-    T = { "<cmd>lua require'telescope.builtin'.current_buffer_tags{}<CR>", "tags buf" },
-    k = { "<cmd>Telescope keymaps<CR>", "keymaps" },
+wk.add({ -- translate visual mode
+  {
+    mode = { "v" },
+    { "<leader>t", group = "trans" },
+    { "<leader>t!r", ":'<,'>TranslateR!<CR>", desc = "replace" },
+    { "<leader>t!w", ":'<,'>TranslateW!<CR>", desc = "window" },
+    { "<leader>tr", ":'<,'>TranslateR<CR>", desc = "replace" },
+    { "<leader>tw", ":'<,'>TranslateW<CR>", desc = "window" },
   },
-}, }, { prefix = "<leader>" })
+})
 
--- translate normal mode
-wk.register({ t = {
-  name = "trans",
-  ["w"]  = { "<cmd>TranslateW<CR>", "word" },
-  ["!w"] = { "<cmd>TranslateW!<CR>", "word" },
-  ["l"]  = { "<cmd>normal V<CR> | :'<,'>TranslateW<CR>",  "line" },
-  ["!l"] = { "<cmd>normal V<CR> | :'<,'>TranslateW!<CR>", "line" },
-  ["r"]  = { "<cmd>normal V<CR> | :'<,'>TranslateR<CR>",  "replace line" },
-  ["!r"] = { "<cmd>normal V<CR> | :'<,'>TranslateR!<CR>", "replace line" },
-}, }, { prefix = "<leader>", mode = "n" })
+wk.add({
+  { "<leader>x", group = "xtab" },
+  { "<leader>xb", group = "buf" },
+  { "<leader>xs", group = "ses" },
+  { "<leader>xt", group = "tab" },
+})
 
--- translate visual mode
-wk.register({ t = {
-  name = "trans",
-  ["w"]  = { ":'<,'>TranslateW<CR>",  "window" },
-  ["!w"] = { ":'<,'>TranslateW!<CR>", "window" },
-  ["r"]  = { ":'<,'>TranslateR<CR>",  "replace" },
-  ["!r"] = { ":'<,'>TranslateR!<CR>", "replace" },
-}, }, { prefix = "<leader>", mode = "v" })
+wk.add({
+  { "<localleader>z", group = "zen" },
+  { "<localleader>zA", "<cmd>TZAtaraxis l10 r10 t3 b1<CR>", desc = "Ataraxis wide" },
+  { "<localleader>zc", "<cmd>TZAtaraxis<CR>", desc = "centered" },
+  { "<localleader>zf", "<cmd>TZFocus<CR>", desc = "focus" },
+  { "<localleader>zm", "<cmd>TZMinimalist<CR>", desc = "minimalist" },
+})
 
-wk.register({ x = {
-  name = "xtab",
-  t = { name = "tab" },
-  b = { name = "buf" },
-  s = { name = "ses" },
-}, }, { prefix = "<leader>" })
-
-wk.register({ z = {
-  name = "zen",
-  A = { "<cmd>TZAtaraxis l10 r10 t3 b1<CR>", "Ataraxis wide" },
-  c = { "<cmd>TZAtaraxis<CR>", "centered" },
-  f = { "<cmd>TZFocus<CR>", "focus" },
-  m = { "<cmd>TZMinimalist<CR>", "minimalist" },
-}, }, { prefix = "<localleader>" })
