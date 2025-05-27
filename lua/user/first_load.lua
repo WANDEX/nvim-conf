cbfc29721aef31d550eec2bfa23d660f15879823
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-05-27 20:43:29 +0300

    [first_load.lua] bootstrap lazy.nvim

--- lua/user/first_load.lua
+++ lua/user/first_load.lua
@@ -1,28 +1,38 @@
-local download_packer = function()
-  if vim.fn.input "Download Packer? (y for yes)" ~= "y" then
+local lazy_path = function()
+  return vim.fn.stdpath("data") .. "/pack/lazy/lazy.nvim"
+end
+
+-- bootstrap lazy.nvim
+local download_lazy_nvim = function()
+  if vim.fn.input "Download lazy.nvim? (y for yes): " ~= "y" then
     return
   end
-
-  local directory = string.format("%s/site/pack/packer/start/", vim.fn.stdpath "data")
-
-  vim.fn.mkdir(directory, "p")
-
-  local out = vim.fn.system(
-    string.format("git clone %s %s", "https://github.com/wbthomason/packer.nvim", directory .. "/packer.nvim")
-  )
-
-  print(out)
-  print "Downloading packer.nvim..."
-  print "( You'll need to restart now )"
-  vim.cmd [[qa]]
+  local lazypath = lazy_path()
+  vim.fn.mkdir(vim.fs.dirname(lazypath), "p")
+  if not vim.uv.fs_stat(lazypath) then
+    print("\nDownloading lazy.nvim...\n")
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    -- print(out)
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+    end
+    vim.cmd([[qa]])
+  end
+  vim.opt.rtp:prepend(lazypath)
 end
 
 return function()
-  if not pcall(require, "packer") then
-    download_packer()
-
+  vim.opt.rtp:prepend(lazy_path())
+  if not pcall(require, "lazy") then
+    download_lazy_nvim()
     return true
   end
-
   return false
 end
