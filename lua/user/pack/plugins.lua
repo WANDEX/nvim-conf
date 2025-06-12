a231a2f84577c9443a54064e2aeb20c58fe72295
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-06-12 15:47:13 +0300

    [plugins.lua] 'mcauley-penney/tidy.nvim' opts & keys

--- lua/user/pack/plugins.lua
+++ lua/user/pack/plugins.lua
@@ -32,9 +32,51 @@ return {
   { 'sbdchd/neoformat' },
   { 'scrooloose/nerdcommenter' },
 
-  { -- trim_trailing_whitespace respecting .editorconfig rules.
-    'mcauley-penney/tidy.nvim', lazy = false, config = true,
+  { -- trim_trailing_whitespace respecting .editorconfig rules. (editorconfig support is built-in neovim).
+    'mcauley-penney/tidy.nvim',
     -- commit = '31f95306ffd408ed4bb5185e8ec3bab9516ad34c', -- 2025 April 14
+    lazy = false,
+    opts = {
+      enabled_on_save = true,
+      -- variable name is misleading! Without it tidy.nvim.toggle() not works!
+      provide_undefined_editorconfig_behavior = true, -- trim ws at EOF
+      -- this exclude - not overrides rules defined in .editorconfig!
+      filetype_exclude = { 'diff', 'markdown' },
+    },
+    config = true,
+    keys = {
+      { -- buffer toggle trimming of whitespaces at the EOF
+        mode = 'n', '<localleader>we', function()
+          local tidy = require('tidy')
+          local opts = tidy.opts
+          local new_state = not opts.provide_undefined_editorconfig_behavior
+          opts.provide_undefined_editorconfig_behavior = new_state
+          tidy.setup(opts)
+          vim.notify("ws trim at EOF = " .. string.format("%s", new_state),
+            vim.log.levels.INFO, {title = "WNDX"}
+          )
+        end, desc = 'ws toggle  at EOF buf',
+      },
+      { -- buffer force one-shot/manual whitespaces trim
+        mode = 'n', '<localleader>wf', function()
+          require('tidy').run({trim_ws = true})
+        end, desc = 'ws force one-shot buf',
+      },
+      { -- buffer toggle trim of whitespaces + toggle built-in neovim property per buffer
+        mode = 'n', '<localleader>wt', function(bufnr)
+          local tidy = require('tidy')
+          if vim.b.editorconfig.trim_trailing_whitespace == nil then
+            return -- cancel => ^ variable not specified in .editorconfig
+          end
+          tidy.toggle()
+          local be_ttw_str = string.format("%s", tidy.opts.enabled_on_save)
+          require('editorconfig').properties.trim_trailing_whitespace(bufnr, be_ttw_str)
+          vim.notify("trim_trailing_whitespace = " .. be_ttw_str,
+            vim.log.levels.INFO, {title = "WNDX"}
+          )
+        end, desc = 'ws toggle trim ws buf',
+      },
+    },
   },
 
   -- syntax
