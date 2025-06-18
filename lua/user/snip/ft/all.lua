ba6352d637cbc1962730e1e4628a55d9a3e93e73
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-06-18 19:39:23 +0300

    [snip][all.lua] added

---
+++ lua/user/snip/ft/all.lua
@@ -0,0 +1,25 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local f = ls.function_node
+local fmt  = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+
+local nl = function(text) -- new line
+  return t { "", text }
+end
+
+-- local uf = require("user.func")
+
+return {
+
+  -- TODO: 1 - comment string based on ft = { lua = '--', cpp = '//' }
+  -- TODO: 2 - take user/repo from .git config - remote
+  s("AUTHOR", fmt("{} AUTHOR: '{}'\n", {
+    i(1, "--"),
+    i(2, "WANDEX/nvim-conf")
+  })),
+
+}
