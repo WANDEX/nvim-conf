4baaf867788513d6c4d7c7459c4f9b40f8abca5a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-06-19 18:20:00 +0300

    vim.lsp.config 'lua_ls'

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -21,6 +21,34 @@ require 'user.diagnostic'
 -- vim.lsp.config('*')
 vim.lsp.config('*', {
   root_markers = { '.git' },
+  -- flags = {
+  --   debounce_text_changes = 150,
+  -- },
+})
+
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = { -- tell the language server which version of Lua you're using
+        version = 'LuaJIT', -- (most likely LuaJIT in the case of Neovim)
+      },
+      diagnostics = {
+        globals = {
+          'vim', -- get the language server to recognize the `vim` global
+          'require'
+        },
+      },
+      workspace = { -- make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      telemetry = {
+        enable = false, -- do not send telemetry data containing a randomized but unique identifier
+      },
+    },
+    -- flags = {
+    --   debounce_text_changes = 150,
+    -- },
+  },
 })
 
 -- luals not works, what is it? standard lua - not nvim?
