ca57ea91dca4ed0d9302a7e0b7461fec74c3c704
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-07-17 16:36:18 +0300

    spec 'mason-org/mason.nvim'

---
+++ lua/user/pack/spec/mason.lua
@@ -0,0 +1,21 @@
+-- AUTHOR: 'WANDEX/nvim-conf'
+-- spec 'mason-org/mason.nvim'
+
+return {
+  'mason-org/mason.nvim',
+  opts = {
+    -- the directory in which to install packages.
+    install_root_dir = require('user.fn').path.concat({ vim.fn.stdpath('data'), 'pack', 'mason' }),
+
+    ---@type '"prepend"' | '"append"' | '"skip"'
+    PATH = "prepend",
+
+    ui = {
+      icons = {
+        package_installed = '',
+        package_pending = '󰅐',
+        package_uninstalled = '󰢘',
+      },
+    },
+  },
+}
