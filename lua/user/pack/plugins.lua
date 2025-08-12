c3d3869af576e0abfb0c7d82b38b3fec3dd78ea5
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-08-12 19:04:57 +0300

    disable 'smoka7/multicursors.nvim' (not that good)

--- lua/user/pack/plugins.lua
+++ lua/user/pack/plugins.lua
@@ -227,33 +227,6 @@ return {
   -- at the end
   { 'neomake/neomake' },
 
-  {
-    'smoka7/multicursors.nvim',
-    -- commit = '72225ea9e4443c3f4b9df91d0193e07c4ee8d382',
-    dependencies = {{
-      'nvimtools/hydra.nvim',
-      -- commit = '8c4a9f621ec7cdc30411a1f3b6d5eebb12b469dc',
-    }},
-    -- version = '*',
-    lazy = false,
-    event = 'VeryLazy',
-    opts = function(_, opts)
-      opts = require('user.pack.conf.multicursors')
-      return opts
-    end,
-    cmd  = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-    keys = {
-      {
-        mode = { 'v', 'n' }, '<C-j>', '<cmd>MCstart<cr>',
-        desc = 'MC Create a selection for selected text or word under the cursor',
-      },
-      {
-        mode = { 'v', 'n' }, '<C-n>', '<cmd>MCunderCursor<cr>',
-        desc = 'MC Select the char under the cursor and start listening for the actions.',
-      },
-    },
-  },
-
   { -- STATUS BAR
     'rebelot/heirline.nvim',
     lazy = false, -- load during startup
