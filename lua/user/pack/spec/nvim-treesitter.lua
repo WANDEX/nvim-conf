90480b228d7e1cf978ddb4b5a2e155e6b0769604
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-11-20 21:55:16 +0300

    [nvim-treesitter.lua] 'ghostbuster91/nvim-next' integration to repeat movements

--- lua/user/pack/spec/nvim-treesitter.lua
+++ lua/user/pack/spec/nvim-treesitter.lua
@@ -14,6 +14,18 @@
 -- https://github.com/latex-lsp/tree-sitter-latex
 
 return {
+  {
+    'ghostbuster91/nvim-next',
+    lazy = false,
+    opts = {
+      default_mappings = {
+        repeat_style = "original",
+      },
+    },
+    config = function(_, opts)
+      require('nvim-next').setup(opts)
+    end,
+  },
 
   {
     'nvim-treesitter/nvim-treesitter',
@@ -129,7 +141,6 @@ return {
     lazy = false,
     opts = {
       textobjects = {
-
         select = {
           enable = true,
           lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
@@ -147,52 +158,57 @@ return {
             ['@class.outer'] = '<c-v>', -- blockwise
           },
           include_surrounding_whitespace = true,
-        }, -- END select
-
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = { query = "@class.outer", desc = "Next class start" },
-            --
-            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-            ["]o"] = "@loop.*",
-            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-            --
-            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-            ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-          -- Below will go to either the start or the end, whichever is closer.
-          -- Use if you want more granular movements
-          -- Make it even more gradual by adding multiple queries and regex.
-          goto_next = {
-            ["]C"] = "@conditional.outer",
-          },
-          goto_previous = {
-            ["[C"] = "@conditional.outer",
-          }
-        }, -- END move
-
+        }, -- END textobjects.select
       }, -- END textobjects
+      nvim_next = { -- nvim-next.integrations!
+        enable = true,
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = { query = "@class.outer", desc = "Next class start" },
+              --
+              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+              ["]o"] = "@loop.*",
+              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+              --
+              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+              ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+              ["]c"] = "@conditional.outer",
+            },
+            goto_previous = {
+              ["[c"] = "@conditional.outer",
+            }
+          }, -- END nvim_next.textobjects.move
+        }, -- END nvim_next.textobjects
+      }, -- END nvim_next
     },
     config = function(_, opts)
+      require('nvim-next.integrations').treesitter_textobjects()
       require('nvim-treesitter.configs').setup(opts)
     end,
+    dependencies = { 'ghostbuster91/nvim-next' } -- repeat movements using: ';' ','.
   },
 
   -- { -- automatically add closing tags for HTML and JSX
