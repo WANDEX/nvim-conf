bc0e3af5d1352ba66db1fd9ca1247dd28a777784
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-11-26 19:54:52 +0300

    [au.lua] get_win_title() moved into 'user.lib.pl'

--- lua/user/au.lua
+++ lua/user/au.lua
@@ -9,6 +9,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- save 'titleold' window title in builtin global variable, set 'titlestring'.
+-- restores old window title if neovim finished working.
 vim.api.nvim_create_autocmd('VimEnter', {
   group    = vim.api.nvim_create_augroup('win_title_upd', { clear = true }),
   pattern  = '*',
@@ -17,21 +18,8 @@ vim.api.nvim_create_autocmd('VimEnter', {
       vim.opt.title       = true
       vim.opt.titleold    = title_old -- set this window title at nvim exit
       vim.opt.titlestring = '%t%( %M%)%( (%{expand("%:~:h")})%)%a (nvim)'
-    end
-    -- TODO: create similar function: wayland_get_win_title()
-    local xorg_get_win_title = function()
-      local ok_id, win_id = pcall(vim.fn.system, { 'xdotool', 'getactivewindow' })
-      if not ok_id then
-        return ''
-      end
-      local ok_name, win_wm_name = pcall(vim.fn.system, { 'xprop', '-id', win_id, 'WM_NAME' })
-      if not ok_name then
-        return ''
-      end -- original window title, before modification by the vim.opt.titlestring.
-      return require('user.fn').split_by(win_wm_name, ' = "')[2]
-    end
-    local orig_title = ''
-    orig_title = xorg_get_win_title()
+    end -- original window title, before modification by the vim.opt.titlestring.
+    local orig_title = require('user.lib.pl').get_win_title()
     if orig_title == '' then
       return
     end
