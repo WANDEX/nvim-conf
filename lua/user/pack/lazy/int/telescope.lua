54e3c7d4d2c90eb0a566710fa8473c9c6dda1f2a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-12-13 07:39:30 +0300

    [telescope.lua] improvements, rewrite spec using lua functions

--- lua/user/pack/lazy/int/telescope.lua
+++ lua/user/pack/lazy/int/telescope.lua
@@ -1,20 +1,75 @@
 -- AUTHOR: 'WANDEX/nvim-conf'
 -- spec 'nvim-telescope/telescope.nvim'
 
-return {
+local M = {}
+
+--- wrapper for the telescope get_dropdown() to merge with default opts.
+--- currently there is no way to set these parameters as default
+--- specifically for the dropdown theme.
+--- (according to the research by: WNDX | date: 2025-12-13 03:41:48 UTC)
+---@param opts? table
+---@return table
+function M.get_dropdown(opts)
+  return require('telescope.themes').get_dropdown(vim.tbl_deep_extend('force', {
+    border = true,
+    previewer = false,
+    shorten_path = false,
+    --- ref layout_config_defaults: telescope.nvim/lua/telescope/config.lua
+    layout_config = {
+      width  = 0.5,
+      height = 0.5,
+      --- when explicitly set - fixes missing borders issue when winblend=0.
+      prompt_position = 'top', -- 'top', 'bottom'
+    },
+  }, opts or {}))
+end
+
+function M.tele_tabby()
+  require('telescope').extensions.tele_tabby.list(M.get_dropdown())
+end
+
+--- call telescope extension dap function based on key.
+---@param key string char
+function M.dap_fn(key)
+  if key == '' or nil then
+    return -- guard
+  end
+  local dropdown_opts = {
+    layout_config = {
+      width  = 0.5,
+      height = 0.95,
+    },
+  }
+  local opts = M.get_dropdown(dropdown_opts)
+  if key == 'b' then
+    require('telescope').extensions.dap.list_breakpoints(opts)
+  elseif key == 'C' then
+    require('telescope').extensions.dap.configurations(opts)
+  elseif key == 'c' then
+    require('telescope').extensions.dap.commands(opts)
+  elseif key == 'f' then
+    require('telescope').extensions.dap.frames(opts)
+  elseif key == 'v' then
+    require('telescope').extensions.dap.variables(opts)
+  else
+    vim.notify(("key '%s' does not have paired command"):format(key), vim.log.levels.ERROR)
+  end
+end
+
+M.spec = {
   'nvim-telescope/telescope.nvim',
   version = '*',
   event = { 'VeryLazy' },
   dependencies = {
+    { 'TC72/telescope-tele-tabby.nvim' }, -- :h :tcd
     { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope-dap.nvim' },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'TC72/telescope-tele-tabby.nvim' },
-    { 'nvim-telescope/telescope-file-browser.nvim' },
+    { 'nvim-telescope/telescope-ui-select.nvim' }, -- sets vim.ui.select to telescope
   },
-  opts = function(_, opts)
-    local actions = require 'telescope.actions'
-    local action_layout = require 'telescope.actions.layout'
-    opts.defaults = {
+  opts = {
+    defaults = {
+      -- winblend = vim.opt.winblend or 0,
       prompt_prefix = '> ',
       selection_caret = '> ',
       entry_prefix = '  ',
@@ -23,7 +78,6 @@ return {
       sorting_strategy = 'descending',
       scroll_strategy = 'cycle',
       color_devicons = true,
-      winblend = 0,
       layout_strategy = 'flex',
       layout_config = {
         width = 0.9,
@@ -43,23 +97,23 @@ return {
       },
       mappings = { -- Global remapping
         i = {
-          ['<M-p>'] = action_layout.toggle_preview,
-          ['<C-n>'] = actions.move_selection_next,
-          ['<C-e>'] = actions.move_selection_previous,
+          ['<M-p>'] = require('telescope.actions.layout').toggle_preview,
+          ['<C-n>'] = require('telescope.actions').move_selection_next,
+          ['<C-e>'] = require('telescope.actions').move_selection_previous,
           ['<C-p>'] = false,
           ['<C-c>'] = false,
-          ['<C-l>'] = actions.close,
+          ['<C-l>'] = require('telescope.actions').close,
         },
         n = {
-          ['n'] = actions.move_selection_next,
-          ['e'] = actions.move_selection_previous,
+          ['n'] = require('telescope.actions').move_selection_next,
+          ['e'] = require('telescope.actions').move_selection_previous,
           ['j'] = false,
           ['k'] = false,
-          ['<C-l>'] = actions.close,
+          ['<C-l>'] = require('telescope.actions').close,
         },
       },
-    }
-    opts.extensions = {
+    },
+    extensions = {
       fzf = {
         fuzzy = true,                     -- false will only do exact matching
         override_generic_sorter = false,  -- override the generic sorter
@@ -69,41 +123,85 @@ return {
       tele_tabby = {
         use_highlighter = true,
       },
+      ['ui-select'] = {
+        M.get_dropdown(),
+      },
     }
-    return opts
-  end,
+  },
   config = function(_, opts)
-    local telescope = require('telescope')
-    telescope.setup(opts)
-    telescope.load_extension 'fzf'          -- fzf-native
-    telescope.load_extension 'file_browser'
+    require('telescope').setup(opts)
+    --- enable Telescope extensions if they are installed
+    ---  dap extension also overrides dap internal ui, so running any dap command,
+    ---  which makes use of the internal ui, will result in a telescope prompt.
+    pcall(require('telescope').load_extension, 'dap')
+    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'ui-select')
+    --- maybe also:
+    --- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
+    --- https://github.com/nvim-telescope/telescope-cheat.nvim
+    --- https://github.com/yorik1984/telescope-cheat-md.nvim
   end,
   keys = {
-    { "<leader>T", "", desc = "Telescope" }, -- group annotation
-    { "<leader>TB", "<cmd>lua require'telescope.builtin'.buffers{}<CR>", desc = "buffers w preview" },
-    { "<leader>TB", "<cmd>lua require'telescope.builtin'.buffers{}<CR>", desc = "buffers w preview" },
-    -- { "<leader>TD", ":lua require'telescope.builtin'.live_grep({search_dirs={ '', }})<C-Left><C-Left><Right>", desc = "grep in list of dirs" },
-    { "<leader>TF", "<cmd>lua require'telescope'.extensions.file_browser.file_browser{ hidden=false, respect_gitignore=false, }<CR>", desc = "Filesystem" },
-    { "<leader>Tb", "<cmd>Telescope buffers theme=dropdown previewer=false border=false<CR>", desc = "buffers" },
-    { "<leader>Tc", "<cmd>lua require'telescope.builtin'.grep_string{}<CR>", desc = "grep under Cursor at cwd" },
-    { "<leader>Td", "<cmd>Telescope diagnostics<CR>", desc = "diagnostics" },
-    { "<leader>Tf", "<cmd>lua require'telescope.builtin'.find_files{}<CR>", desc = "find file at cwd" },
-    { "<leader>Tg", "<cmd>lua require'telescope.builtin'.live_grep{}<CR>", desc = "grep at cwd" },
-    { "<leader>To", "<cmd>lua require'telescope.builtin'.live_grep{ grep_open_files=true }<CR>", desc = "grep Opened files" },
-    { "<leader>Tt", "<cmd>Telescope tele_tabby list theme=dropdown previewer=false border=false<CR>", desc = "tabs" },
-    { "<leader>Tv", "", desc = "vim" }, -- group annotation
-    { "<leader>TvC", "<cmd>lua require'telescope.builtin'.colorscheme{}<CR>", desc = "colorscheme" },
-    { "<leader>TvH", "<cmd>lua require'telescope.builtin'.highlights{}<CR>", desc = "Highlights" },
-    { "<leader>TvT", "<cmd>lua require'telescope.builtin'.current_buffer_tags{}<CR>", desc = "tags buf" },
-    { "<leader>Tvc", "<cmd>lua require'telescope.builtin'.command_history{}<CR>", desc = "command history" },
-    { "<leader>Tvf", "<cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find{}<CR>", desc = "fuzzy find buf" },
-    { "<leader>Tvh", "<cmd>lua require'telescope.builtin'.help_tags{}<CR>", desc = "help tags" },
-    { "<leader>Tvk", "<cmd>Telescope keymaps<CR>", desc = "keymaps" },
-    { "<leader>Tvm", "<cmd>lua require'telescope.builtin'.man_pages{}<CR>", desc = "man pages" },
-    { "<leader>Tvo", "<cmd>lua require'telescope.builtin'.vim_options{}<CR>", desc = "options vim edit" },
-    { "<leader>Tvp", "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>", desc = "prev opened files" },
-    { "<leader>Tvr", "<cmd>lua require'telescope.builtin'.registers{}<CR>", desc = "registers" },
-    { "<leader>Tvs", "<cmd>lua require'telescope.builtin'.search_history{}<CR>", desc = "search history" },
-    { "<leader>TG", "", desc = "Generate/GrugFar" }, -- group annotation
+    { '<leader>T',  '', desc = 'Telescope' }, -- group annotation
+    { '<leader>Ta', '', desc = '[DAP] commands' },
+    { '<leader>Tab', function() M.dap_fn('b') end, desc = 'breakpoints' },
+    { '<leader>TaC', function() M.dap_fn('C') end, desc = 'Configurations' },
+    { '<leader>Tac', function() M.dap_fn('c') end, desc = 'commands' },
+    { '<leader>Taf', function() M.dap_fn('f') end, desc = 'frames' },
+    { '<leader>Tav', function() M.dap_fn('v') end, desc = 'variables' },
+    { '<leader>TB', require('telescope.builtin').buffers, desc = 'buffers preview' },
+    {
+      '<leader>Tb', function()
+        require('telescope.builtin').buffers(M.get_dropdown())
+      end, desc = 'buffers'
+    },
+    { --- shortcut for searching nvim configuration files
+      '<leader>TC', function()
+        require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })
+      end, desc = 'grep Config (nvim)'
+    },
+    { '<leader>Tc', require('telescope.builtin').grep_string, desc = 'grep under cursor at cwd' },
+    --[[
+    { '<leader>TD',
+        ":lua require('telescope.builtin').live_grep(" ..
+        "{search_dirs={ '', }})<C-Left><C-Left><Right>", --- shift cursor inside ''
+      desc = 'grep in list of dirs'
+    },
+    --]]
+    { --- same as above but using lua nvim api functions! (left for the future reference)
+      '<leader>TD', function()
+        local lua_fun = "require('telescope.builtin').live_grep({ search_dirs={ '', } })"
+        local key_seq = '<C-Left><C-Left><C-Left><Right>' -- shift cursor inside ^ == ''
+        local key_cod = vim.api.nvim_replace_termcodes(key_seq, true, false, true)
+        local res_str = ":lua " .. lua_fun
+        vim.api.nvim_feedkeys(res_str, 'n',   true)
+        vim.api.nvim_feedkeys(key_cod, 'nx!', true)
+      end, desc = 'grep in list of dirs'
+    },
+    { '<leader>Td', require('telescope.builtin').diagnostics, desc = 'diagnostics' },
+    { '<leader>Tf', require('telescope.builtin').find_files,  desc = 'find file at cwd' },
+    { '<leader>Tg', require('telescope.builtin').live_grep,   desc = 'grep at cwd' },
+    {
+      '<leader>To', function()
+        require('telescope.builtin').live_grep({ grep_open_files=true })
+      end, desc = 'grep opened files'
+    },
+    { '<leader>Tt', M.tele_tabby, desc = 'tabs' },
+    { '<leader>Tv', '', desc = 'vim' }, -- group annotation
+    { '<leader>TvC', require('telescope.builtin').colorscheme,                desc = 'colorscheme' },
+    { '<leader>TvH', require('telescope.builtin').highlights,                 desc = 'Highlights' },
+    { '<leader>TvT', require('telescope.builtin').current_buffer_tags,        desc = 'tags buf' },
+    { '<leader>Tvc', require('telescope.builtin').command_history,            desc = 'command history' },
+    { '<leader>Tvf', require('telescope.builtin').current_buffer_fuzzy_find,  desc = 'fuzzy find buf' },
+    { '<leader>Tvh', require('telescope.builtin').help_tags,                  desc = 'help tags' },
+    { '<leader>Tvk', require('telescope.builtin').keymaps,                    desc = 'keymaps' },
+    { '<leader>Tvm', require('telescope.builtin').man_pages,                  desc = 'man pages' },
+    { '<leader>Tvo', require('telescope.builtin').vim_options,                desc = 'options vim edit' },
+    { '<leader>Tvp', require('telescope.builtin').oldfiles,                   desc = 'prev opened files' },
+    { '<leader>Tvr', require('telescope.builtin').registers,                  desc = 'registers' },
+    { '<leader>Tvs', require('telescope.builtin').search_history,             desc = 'search history' },
+    { '<leader>TG', '', desc = 'Generate/GrugFar' }, -- group annotation
   },
 }
+
+return M.spec
