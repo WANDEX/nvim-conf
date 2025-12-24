fa468b6154d15f811bf551ed9c7c62369ea51662
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-12-25 00:58:22 +0300

    [lazy.lua] change_detection.enabled = false -- do not auto-reload

--- lua/user/pack/lazy/lazy.lua
+++ lua/user/pack/lazy/lazy.lua
@@ -21,6 +21,10 @@ function M.load(opts)
       log = { '-8' }, -- show last 8 commits
     },
     checker = { enabled = false },
+    change_detection = {
+      enabled = false, -- automatically check for config file changes and reload the ui
+      notify = true, -- get a notification when changes are found
+    },
     diff = { -- diff command <d> can be one of:
       -- * git: will run git diff and open a buffer with filetype git
       -- * diffview.nvim: will open Diffview to show the diff
