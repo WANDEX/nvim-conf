feb7e05f176f0ee10522ff48bd3b24438c5d52c6
WANDEX <15724752+WANDEX@users.noreply.github.com>
2026-02-06 01:51:01 +0300

    [debug.lua] dap.configurations codelldb c/cpp/rust re-use

--- lua/user/pack/lazy/oth/debug.lua
+++ lua/user/pack/lazy/oth/debug.lua
@@ -3,6 +3,9 @@
 --
 -- ref:
 -- https://github.com/nvim-lua/kickstart.nvim/blob/3338d3920620861f8313a2745fd5d2be39f39534/lua/kickstart/plugins/debug.lua#L11
+--
+-- configurations ref:
+-- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 
 return {
   'mfussenegger/nvim-dap',
@@ -26,15 +29,27 @@ return {
     },
     {
       '<F17>', function() -- S-F5
-        require('dap').stop()
+        require('dap').close() -- dap.stop() is deprecated!
       end,
-      desc = 'Debug: Start/Continue',
+      desc = 'Debug: Stop',
+    },
+    {
+      '<F29>', function() -- C-F5
+        require('dap').reverse_continue()
+      end,
+      desc = 'Debug: Stop',
     },
     { --- last session result, see session output in case of unhandled exception.
       '<F7>', function()
         require('dapui').toggle()
       end,
-      desc = 'Debug: See last session result.',
+      desc = 'Debug: Toggle last session ui view',
+    },
+    {
+      '<F19>', function() -- S-F7
+        require('dap').run_last()
+      end,
+      desc = 'Debug: Run last session again',
     },
     {
       '<leader>ab', function()
@@ -55,10 +70,22 @@ return {
       desc = 'Debug: Set Breakpoint',
     },
     {
+      '<F33>', function() -- C-F9
+        require('dap').run_to_cursor()
+      end,
+      desc = 'Debug: Continue execution to the current cursor',
+    },
+    {
       '<F10>', function()
         require('dap').step_over()
       end,
       desc = 'Debug: Step Over',
+    },
+    {
+      '<F22>', function() -- S-F10
+        require('dap').step_back()
+      end,
+      desc = 'Debug: Step Back', -- rr
     },
     {
       '<F11>', function()
@@ -74,8 +101,7 @@ return {
     },
   },
   config = function()
-    local dap   = require 'dap'
-    local dapui = require 'dapui'
+    local dap, dapui = require('dap'), require('dapui')
 
     require('mason-nvim-dap').setup {
       --- Makes a best effort to setup the various debuggers with reasonable debug configurations.
@@ -86,9 +112,8 @@ return {
       ensure_installed = {},
     }
 
-    --- Dap UI setup -- For more information, see |:help nvim-dap-ui|
-    ---@diagnostic disable-next-line: missing-fields
-    dapui.setup {
+    ---@type dapui.Config
+    local dapui_opts = { --- Dap UI setup -- For more information, see |:help nvim-dap-ui|
       controls = {
         element = 'repl',
         enabled = true,
@@ -109,7 +134,7 @@ return {
       floating = {
         border = 'single',
         mappings = {
-          close = { 'q', '<Esc>' },
+          close = { 'q', '<Esc>', '<C-l>' },
         },
       },
       force_buffers = true,
@@ -120,29 +145,29 @@ return {
       },
       layouts = { {
         elements = { {
-          id = "scopes",
+          id = 'scopes',
           size = 0.25,
         }, {
-          id = "breakpoints",
+          id = 'breakpoints',
           size = 0.25,
         }, {
-          id = "stacks",
+          id = 'stacks',
           size = 0.25,
         }, {
-          id = "watches",
+          id = 'watches',
           size = 0.25,
         }, },
-        position = "left",
+        position = 'left',
         size = 60,
       }, {
         elements = { {
-          id = "repl",
+          id = 'repl',
           size = 0.5,
         }, {
-          id = "console",
+          id = 'console',
           size = 0.5,
         }, },
-        position = "bottom",
+        position = 'bottom',
         size = 10,
       }, },
       mappings = {
@@ -159,8 +184,9 @@ return {
       },
     }
 
-    --- change breakpoint icons
-    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    dapui.setup(dapui_opts)
+
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' }) --- change breakpoint icons
     vim.api.nvim_set_hl(0, 'DapStop',  { fg = '#ffcc00' })
     local breakpoint_icons = vim.g.NF
       and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
@@ -171,9 +197,86 @@ return {
       vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
     end
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    --- use nvim-dap events to open and close the windows automatically (:help dap-extensions)
+    dap.listeners.after.event_initialized.dapui_config = dapui.open
+    dap.listeners.before.attach.dapui_config           = dapui.open
+    dap.listeners.before.event_exited.dapui_config     = dapui.close
+    dap.listeners.before.event_terminated.dapui_config = dapui.close
+    dap.listeners.before.launch.dapui_config           = dapui.open
+
+    -- vim.keymap.set({ 'n', 'v' }, '<M-k>', function()
+    --   dapui.eval('<cword>')
+    --   -- dapui.eval('<expression>')
+    --   -- dapui.eval()
+    -- end, { desc = 'evaluate expression', silent = false, expr = true })
+
+    --- https://github.com/rcarriga/nvim-dap-ui#floating-elements
+    -- vim.keymap.set({ 'n', 'v' }, '<M-k>', function()
+    --   dapui.float_element()
+    -- end, { desc = 'dapui: float element', silent = true })
+
+    vim.keymap.set({ 'n', 'v' }, '<localleader>D', function()
+      dapui.setup(dapui_opts)
+      -- dapui.update_render(dapui_opts.render)
+      dapui.toggle()
+    end, { desc = 'dapui: restore layouts', silent = true })
+
+    dap.configurations.cpp = {
+      {
+        name = 'pick process | codelldb |',
+        type = 'codelldb',
+        request = 'attach',
+        program = '${command:pickProcess}',
+      },
+      {
+        name = 'pick binary  | codelldb |',
+        type = 'codelldb',
+        request = 'launch',
+        program = '${command:pickFile}',
+      },
+      {
+        name = 'pick binary  | codelldb | wndx_cmake_build.sh |',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          local bdir = require('user.lib.fn').path.wndx_cmake_build()
+          local path = require('dap.utils').pick_file({
+            path = bdir, executables = true,
+            filter = function(fpath)
+              return
+                not vim.endswith(fpath, '.out') and -- CompilerIdCXX/a.out
+                not vim.endswith(fpath, '.bin')     -- CMakeDetermineCompilerABI_CXX.bin
+            end,
+          })
+          return (path and path ~= '') and path or dap.ABORT
+        end,
+      },
+      {
+        name = 'pick binary  | codelldb | wndx_cmake_build.sh | --gtest_filter=*',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          local bdir = require('user.lib.fn').path.wndx_cmake_build()
+          local path = require('dap.utils').pick_file({
+            path = bdir, executables = true,
+            filter = function(fpath)
+              return
+                not vim.endswith(fpath, '.out') and -- CompilerIdCXX/a.out
+                not vim.endswith(fpath, '.bin')     -- CMakeDetermineCompilerABI_CXX.bin
+            end,
+          })
+          return (path and path ~= '') and path or dap.ABORT
+        end,
+        args = function()
+          vim.env.GTEST_TESTS_FILTER = vim.env.GTEST_TESTS_FILTER or '*' -- restore last input
+          local input = vim.fn.input({ prompt = '--gtest_filter=', default = vim.env.GTEST_TESTS_FILTER })
+          vim.env.GTEST_TESTS_FILTER = input -- save last input
+          return { '--gtest_brief=1', '--gtest_filter=' .. input, }
+        end,
+      },
+    } -- END dap.configurations.cpp
+    dap.configurations.c    = dap.configurations.cpp -- re-use the configuration
+    dap.configurations.rust = dap.configurations.cpp
 
     --[[
     require('dap-go').setup { -- Install golang specific config
@@ -184,8 +287,6 @@ return {
       },
     }
     --]]
-
-    -- FIXME: build/ dir is ignored and cannot be used as path in cmd mode autocompletion.
 
   end,
 }
