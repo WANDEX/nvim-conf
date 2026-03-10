ac97cfa7b2dc06bd08d79a88f1c6278463a22eac
WANDEX <15724752+WANDEX@users.noreply.github.com>
2026-03-10 23:59:59 +0300

    [au.lua] disable automatic commenting on newline

--- lua/user/wndx/au.lua
+++ lua/user/wndx/au.lua
@@ -1,23 +1,33 @@
 -- AUTHOR: 'WANDEX/nvim-conf'
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group    = vim.api.nvim_create_augroup('hi_on_yank', { clear = true }),
+  group    = vim.api.nvim_create_augroup('wndx_hi_on_yank', { clear = true }),
   pattern  = '*',
   callback = function()
     vim.highlight.on_yank({ timeout=1500 })
   end,
 })
 
+--- disable automatic commenting on newline
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group    = vim.api.nvim_create_augroup('wndx_formatoptions', { clear = true }),
+  pattern  = '*',
+  callback = function()
+    -- vim.opt.formatoptions:remove({ 'c', 'o', 'r' })
+    vim.opt.formatoptions:remove({ 'o' })
+  end,
+})
+
 --- override theme highlights with the user highlights.
 vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-  group    = vim.api.nvim_create_augroup('hi_override_wndx', { clear = true }),
+  group    = vim.api.nvim_create_augroup('wndx_hi_override', { clear = true }),
   callback = require('user.wndx.hi').main,
 })
 
 --- save 'titleold' window title in builtin global variable, set 'titlestring'.
 --- restores old window title if neovim finished working.
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  group    = vim.api.nvim_create_augroup('win_title_upd', { clear = true }),
+  group    = vim.api.nvim_create_augroup('wndx_win_title_upd', { clear = true }),
   callback = function()
     local upd_win_title = function(title_old)
       vim.opt.title       = true
